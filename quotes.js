dfea0f1d1f5738d41e0c477184f1fd0eb287b973
Martin <simplisticmartin@gmail.com>
2020-02-13 00:05:53 -0500

    inspiration-quote-applicaiton

---
+++ quotes.js
@@ -0,0 +1,33 @@
+let quotes = [
+{
+    quote : "History is written by victors",
+    date: "12/30/2019",
+    author: "Winston Churchill",
+    picUrl: "img/churchill.jpg",
+    wikiLink: "",
+    meaningPage: ""
+
+},
+{
+
+    quote : "The Law of Serendipity: Lady Luck favors the one who tries",
+    date: "sometime/2017",
+    author: "Barbara Oakley",
+    picUrl: "img/Boakley.jpg",
+    wikiLink: "",
+    meaningPage: ""
+
+},
+{
+
+    quote : "Work Hard, Be Kind and amazing Things will happen",
+    date: "sometime/2017",
+    author: "Conan O'Brien",
+    picUrl: "img/conan.jpg",
+    wikiLink: "",
+    meaningPage: ""
+
+}
+]
+
+console.log(quotes)
---
+++ shoot.js
@@ -0,0 +1,14 @@
+//let chips = fetch('quotes.json')
+//.then(data => data.json()).then(json => re);
+/*
+let jsonData;
+
+fetch("quotes.json").then(function(u){
+    return u.json();
+}).then(function(json){
+    jsonData = json;
+});
+
+console.log(jsonData)
+    //json => console.log(json));
+*/
